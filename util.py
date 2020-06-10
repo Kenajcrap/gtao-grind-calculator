bc7e2baaba45a32f40a835c9315a647354d56ff0
Kenajcrap <fael-1999@hotmail.com>
2020-06-10 01:57:36 -0300

    simple mission data JSON decoder, might have to change this later

--- util.py
+++ util.py
@@ -1,4 +1,16 @@
 import json
+import main
 
-def decode_missions(dct):
-	pass
+def decode_mission(dct):
+	if "name" in dct:
+		miss = main.mission(dct["name"], dct["reward"], dct["expectedduration"])
+		if "cooldown" in dct:
+			for i in dct["cooldown"]:
+				miss.setcooldown(i, dct["cooldown"][i])
+		return miss
+	else:
+		return dct
+
+with open("missions.json") as missions_data:
+	data = missions_data.read()
+	missions = json.loads(data, object_hook=decode_mission)
