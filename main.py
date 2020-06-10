329f8704110f4592f441c3e68aaa587e0a482b0b
Kenajcrap <fael-1999@hotmail.com>
2020-06-09 23:09:22 -0300

    add cooldown related methods to mission calss

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 class mission:
 
-	def __init__(self, reward, expectedduration, cooldownonfinish):
+	def __init__(self, reward, expectedduration):
 		self.reward = reward
 		self.expectedduration = expectedduration
-		self.cooldown = {}
-		self.cooldown.onfinish = cooldownonfinish
+		self.cooldownremaining = 0
+
+	def setcooldown(self, seconds, ontrigger):
+		if ontrigger:
+			self.cooldownontrigger = seconds
+		else:
+			self.cooldownonfinish = seconds
+
+	def finish(self):
+		try:
+			if self.cooldownremaining < self.cooldownonfinish:
+				self.cooldownremaining = self.cooldownonfinish
+		except AttributeError:
+			pass
+
+	def trigger(self):
+		try:
+			if self.cooldownremaining < self.cooldownontrigger:
+				self.cooldownremaining = self.cooldownontrigger
+		except AttributeError:
+			pass
+
+
+class supplymission(mission):
+
+	pass
+
+class deliverymission(mission):
+	
+	pass
